32e56791148d39b295206d9a50a5c10fa2472b0d
oPiloto <pedro.piloto937@gmail.com>
2024-05-07 02:28:17 -0300

    jogo finalizado

---
+++ Scripts/dialog_box.gd
@@ -0,0 +1,55 @@
+extends MarginContainer
+
+@onready var text_label = $Label_margin/text_label
+@onready var letter_timer_display = $letter_timer_display
+
+const MAX_WIDTH = 256
+
+var text = ""
+var letter_index = 0 
+
+var letter_display_timer := 0.10
+var space_display_timer := 0.08
+var punctuaction_display_timer := 0.5
+
+signal text_display_finished()
+
+func display_text(text_to_display: String): 
+	text = text_to_display
+	text_label.text = text_to_display
+	
+	await resized
+	
+	custom_minimum_size.x = min(size.x, MAX_WIDTH)
+	
+	if size.x > MAX_WIDTH:
+		text_label.autowrap_mode = TextServer.AUTOWRAP_WORD
+		await resized #aguardar redimencionamento em x
+		await resized #aguardar redimencionamento em y
+		custom_minimum_size.y = size.y
+		
+	global_position.x -= size.x / 2 
+	global_position.y -= size.y + 24 / 2
+	text_label.text = ""
+	display_letter()
+	
+
+func display_letter():
+	text_label.text += text[letter_index]
+	letter_index += 1 
+	
+	if letter_index >= text.length():
+		text_display_finished.emit()
+		return
+	
+	match text[letter_index]:
+		"!", "?", ",", ".":
+			letter_timer_display.start(punctuaction_display_timer)
+		" ":
+			letter_timer_display.start(space_display_timer)
+		_:
+			letter_timer_display.start(letter_display_timer)
+
+
+func _on_letter_timer_display_timeout():
+	display_letter()
